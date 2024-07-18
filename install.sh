04e4a1ad0485ed760b881ae3dc40d1aa9c6ad2ad
sf3ris <sf3ris@sf3ris.>
2024-07-18 15:18:49 +0200

    feat: improve dotfile management

--- install.sh
+++ install.sh
@@ -1,7 +1,10 @@
 #! /bin/bash
 
 backup_dir=~/dotfiles_backup
-files="zshrc config/nvim/templates config/nvim/lua tmux.conf tmux"
+echo "Cleaning up backup directory"
+rm -rf $backup_dir/*
+
+files="zshrc config/nvim/templates config/nvim/lua tmux.conf tmux/plugins yabairc skhdrc"
 current_dir=$(pwd)
 
 echo "Creating $backup_dir to backup current dotfiles"
@@ -12,7 +15,7 @@ cd ~
 for file in $files; do
   echo "================= $file ================================="
   echo "Moving current $file dotfile to $backup_dir"
-  mv ~/.$file $backup_dir
+  mv ~/.$file $backup_dir/
 
   echo "Creating symlink between $current_dir/$file with ~/.$file"
   ln -s $current_dir/$file ~/.$file
